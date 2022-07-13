336b4299d2ad45686efe247b3585a54512d16ffe
anastasiia <anastasiia_yarova@epam.com>
2022-07-13 21:47:28 +0300

    commit

--- src/test/java/io/projectName/runers/RunWithClucumber.java
+++ src/test/java/io/projectName/runers/RunWithClucumber.java
@@ -1,4 +1,5 @@
 package io.projectName.runers;
+
 import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
@@ -8,7 +9,7 @@ import org.junit.runner.RunWith;
         features = {"src/test/resources/featureFiles"},
         glue = {"io.projectName.steps"},
         monochrome = true,
-        plugin = {"json:target/cucumber-report/cucumber.json"}
+        plugin = {"pretty", "json:target/cucumber.json"}
 )
 public class RunWithClucumber {
 
