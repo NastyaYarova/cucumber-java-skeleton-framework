b615e9a3fb0ede8bf5f0b234e54fb75391a5c192
anastasiia <anastasiia_yarova@epam.com>
2022-07-16 15:32:21 +0300

    commit

--- src/test/java/io/projectName/runers/TestRunnerTestNG.java
+++ src/test/java/io/projectName/runers/TestRunnerTestNG.java
@@ -3,14 +3,11 @@ package io.projectName.runers;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-//@CucumberOptions(
-//        features = "src/test/resources/featureFiles",
-//        glue = {"io.projectName.steps"},
-//        tags = {"not @Ignore", "@Smoke"},
-//        plugin = {"pretty", "json:target/cucumber-reports/Cucumber.json",
-//                "junit:target/cucumber-reports/Cucumber.xml",
-//                "html:target/cucumber-reports"}
-//)
-public class TestRunnerTestNG {
-//        extends AbstractTestNGCucumberTests {
+@CucumberOptions(
+        features = "src/test/resources/featureFiles",
+        glue = {"io.projectName.steps"},
+        tags = {"not @Ignore", "@Smoke"},
+        plugin = {"pretty", "json:target/cucumber-report/cucumber.json"}
+)
+public class TestRunnerTestNG extends AbstractTestNGCucumberTests {
 }
